8c458df59bc69833bff9f069448f483185770e8c
PrakharGupta36 <prakhargupta3680@gmail.com>
2022-09-30 15:56:50 +0530

    Added Post Processing

--- src/components/ScrollPage.jsx
+++ src/components/ScrollPage.jsx
@@ -10,7 +10,7 @@ export default function ScrollPage() {
       <ScrollControls
         horizontal
         pages={isMobile ? 30 : isDesktop ? 8.9 : null}
-        distance={1}
+        distance={isMobile ? 0.5 : 1}
         damping={10}>
         {useModels().map((e) => {
           const {
