9307d9b09a7b3a918b478135e1ace955f34293af
PrakharGupta36 <prakhargupta3680@gmail.com>
2022-09-30 16:26:15 +0530

    Added a scroll bar

--- src/components/ScrollPage.jsx
+++ src/components/ScrollPage.jsx
@@ -9,7 +9,7 @@ export default function ScrollPage() {
     <>
       <ScrollControls
         horizontal
-        pages={isMobile ? 30 : isDesktop ? 8.9 : null}
+        pages={isMobile ? 28 : 9}
         distance={isMobile ? 0.5 : 1}
         damping={10}>
         {useModels().map((e) => {
