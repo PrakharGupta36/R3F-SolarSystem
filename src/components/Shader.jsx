209e9943f566edb826d0f671009258b60f113603
PrakharGupta36 <prakhargupta3680@gmail.com>
2022-10-08 04:43:16 +0530

    Fix mobile damping

--- src/components/Shader.jsx
+++ src/components/Shader.jsx
@@ -7,7 +7,6 @@ import {
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { useRef } from "react";
-import { isMobile } from "react-device-detect";
 
 export default function Shader({ position, img }) {
   const ref = useRef();
@@ -19,8 +18,8 @@ export default function Shader({ position, img }) {
   return (
     <>
       <mesh position={position} scale={[2, 4, 1]}>
-        <planeGeometry args={[isMobile ? 1.5 : 1.8, 0.6, 32, 32]} />
-        <MeshDistortMaterial ref={ref} speed={2} map={texture} />
+        <planeGeometry args={[1, 1, 32, 32]} />
+        <MeshDistortMaterial ref={ref} speed={5} map={texture} />
       </mesh>
       <SpotLight
         position={[120, 0, 4]}
